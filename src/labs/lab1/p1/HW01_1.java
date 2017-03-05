4aa0d9336421af878fd61860139a15e9eadcc640
Doozy <slobodan.kletnikov@gmail.com>
2017-03-05 22:58:54 +0100

    Doozy

---
+++ src/labs/lab1/p1/HW01_1.java
@@ -0,0 +1,13 @@
+package labs.lab1.p1;
+
+/**
+ Task 1 Problem 1
+ Write a Java program which will show the average size of the all .txt files which are part of the directory denoted as a command line argument.
+
+ Note: Use the File class to access the content of the directory.
+
+ Solution: The HW01_1.java file from your solution should be placed here, using copy-paste.
+ */
+public class HW01_1 {
+	
+}
